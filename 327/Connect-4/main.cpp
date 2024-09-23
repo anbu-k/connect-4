7ba0b3b8388fa79537065bb72625fdcafd0b76f4
anbu <anbu@iastate.edu>
2024-09-23 14:12:03 -0500

    final product

---
+++ 327/Connect-4/main.cpp
@@ -0,0 +1,14 @@
+/*
+ * Author: Anbu Krishnan
+ * Main entry point 
+ */
+
+#include <iostream>
+#include "game.h"
+
+int main() {
+    Game connectFour;
+    while (connectFour.playGame()) {
+    }
+    return 0;
+}
